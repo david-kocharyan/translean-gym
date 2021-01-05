c44d359672733e023467b987914d6ffb3ee0c704
Harut <harut.soghomonyan@gmail.com>
2021-01-05 13:13:56 +0400

    Fix nameing

--- database/migrations/2021_01_05_090311_add_sleep_time_column_to_users_table.php
+++ database/migrations/2021_01_05_090311_add_sleep_time_column_to_users_table.php
@@ -14,7 +14,7 @@ class AddSleepTimeColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('wake_up_time', 191)->nullable();
+            $table->string('sleep_time', 191)->nullable();
         });
     }
 
