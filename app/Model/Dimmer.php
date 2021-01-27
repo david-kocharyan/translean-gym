cc6e5b335229660f29112330a9aa0f1c0befd9b9
Harut <harut.soghomonyan@gmail.com>
2021-01-27 06:38:36 -0500

    Dimmer fillable

--- app/Model/Dimmer.php
+++ app/Model/Dimmer.php
@@ -8,6 +8,10 @@ class Dimmer extends Model
 {
     protected $table = "dimmer";
     public $timestamps = false;
+    
+    protected $fillable = [
+        'user_id','date', 'dimmer'
+    ];
 
     public function resolveChildRouteBinding($childType, $value, $field)
     {
